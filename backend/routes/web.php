adf5d88555726c14dc59c2213bd0c570b54f53b8
LeeAnhDevXplorer <danh.alee13@gmail.com>
2024-11-27 12:10:59 +0700

    Update

---
+++ backend/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']);         // Lấy tất cả người dùng
+    Route::post('/', [UserController::class, 'store']);        // Tạo mới người dùng
+    Route::get('{id}', [UserController::class, 'show']);       // Lấy thông tin người dùng theo ID
+    Route::put('{id}', [UserController::class, 'update']);     // Cập nhật thông tin người dùng
+    Route::delete('{id}', [UserController::class, 'destroy']); // Xóa người dùng
+});
